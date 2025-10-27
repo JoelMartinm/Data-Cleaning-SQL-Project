f9ef9207bec5761f6e0da47f5292478f7a076dc2
Joel Martin <joelmartinmm00@gmail.com>
2025-10-28 10:19:27 +1100

    Add files via upload

---
+++ DATA CLEANING PROJECT SQL.sql
@@ -0,0 +1,173 @@
+-- DATA CLEANING
+
+SELECT * 
+FROM layoffs; 
+
+-- Creating a staging table
+
+CREATE TABLE layoffs_staging
+LIKE layoffs;
+
+-- Inserting raw data to staging table
+
+INSERT layoffs_staging
+SELECT *
+FROM layoffs;
+
+--
+SELECT *,
+ROW_NUMBER() OVER(PARTITION BY company, industry, total_laid_off, percentage_laid_off, `date`) AS row_num2
+FROM layoffs_staging2; 
+
+WITH duplicate_cte AS
+(
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
+FROM layoffs_staging2 
+)
+SELECT *
+FROM duplicate_cte
+WHERE row_num > 1;
+
+SELECT * 
+FROM layoffs_staging
+WHERE company ='Casper'; 
+
+WITH duplicate_cte2 AS
+(
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num2
+FROM layoffs_staging2 
+)
+
+DELETE 
+FROM  layoffs_staging2
+WHERE row_num2 > 1;
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` bigint DEFAULT NULL,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+SELECT * 
+FROM layoffs_staging2
+WHERE row_num >1;
+
+INSERT INTO layoffs_staging2
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num2
+FROM layoffs_staging;
+
+
+DELETE 
+FROM layoffs_staging2
+WHERE row_num2 >1 ;
+
+SELECT * 
+FROM layoffs_staging2
+WHERE row_num >0 ;
+
+-- STANDARDIZING DATA
+
+SELECT company,TRIM(company)
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET company = TRIM(company);
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry LIKE 'crypto%';
+
+UPDATE layoffs_staging2
+SET industry = 'Crypto'
+WHERE industry LIKE 'crypto%';
+
+SELECT DISTINCT country, TRIM(TRAILING '.' FROM country)
+FROM layoffs_staging2
+ORDER BY 1;
+
+UPDATE layoffs_staging2
+SET country = TRIM(TRAILING '.' FROM country)
+WHERE country LIKE 'United States%';
+
+-- TEXT TO DATE
+
+SELECT `date`
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET `DATE` = STR_TO_DATE(`DATE`,'%m/%d/%Y');
+
+ALTER TABLE layoffs_staging2date
+MODIFY COLUMN `date` DATE;
+
+-- changing null values 
+
+SELECT * 
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT * 
+FROM layoffs_staging2
+WHERE industry IS NULL
+OR industry = '';
+
+SELECT * 
+FROM layoffs_staging2
+WHERE company = 'Airbnb';
+
+UPDATE layoffs_staging2
+SET industry = NULL  
+WHERE industry= '';
+
+SELECT *
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+ ON t1.company = t2.company
+ WHERE (t1.industry IS NULL OR t1.industry = '')
+ AND t2.industry IS NOT NULL; 
+ 
+ UPDATE layoffs_staging2 t1
+ JOIN layoffs_staging2 t2
+   ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE (t1.industry IS NULL OR t1.industry = '')
+ AND t2.industry IS NOT NULL;
+ 
+ SELECT * 
+ FROM layoffs_staging2
+
+SELECT * 
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+DELETE 
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT * 
+FROM layoffs_staging2;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
+
+
+
+
+
+ 
